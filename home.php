ca5911c4518838a5d21db8ad008fee07624f1736
bjorn sodergren <bjorn.sodergren@gmail.com>
2022-11-03 23:26:41 -0500

    gfdfd

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-DEFINE('__SCRIPT_NAME__', basename($_SERVER['PHP_SELF'], ".php") );
+DEFINE('__SCRIPT_NAME__', basename($_SERVER['PHP_SELF'], ".php"));
 
 require_once("_config.inc.php");
 
@@ -7,54 +7,52 @@ define('TITLE', "Home");
 
 include __LAYOUT_HEADER__;
 
-	
-		
-	$sql = "select count(studio) as cnt, studio from ".Db_TABLE_FILEDB." WHERE library = '".$in_directory."' GROUP by studio ORDER BY `studio` ASC;";
-	$result = $db->query($sql);
+$sql = "select count(studio) as cnt, studio from " . Db_TABLE_FILEDB . " WHERE library = '" . $in_directory . "' GROUP by studio ORDER BY `studio` ASC;";
+$result = $db->query($sql);
 
-	
+$sql = query_builder("DISTINCT(library) as library ");
+$result2 = $db->query($sql);
+
 ?>
-    
-    <main role="main" class="container">
 
-    <?php
-	
+<main role="main" class="container">
+
+	<?php
+	foreach ($result2 as $k => $v) {
+		echo "<a href='home.php?library=" . $v["library"] . "'>" . $v["library"] . "</a> | ";
+	}
 	echo "<ul> \n";
-	foreach($result as $k => $v )
-	{
-		
-		if($v["studio"] != "" ){
-		$cnt = $v["cnt"];
-			
-			$query='select count(studio_a) as cnt, studio_a from '.Db_TABLE_FILEDB.' WHERE studio like "'.$v['studio'].'" GROUP by studio_a ORDER BY `studio_a` ASC';
-			$alt_result = $db->query($query);
-			
-			$studio = str_replace(" ","-",$v['studio']);
-			$studio = str_replace("/","_",$studio);
-			
-			echo "<li><a href='studio.php?viewstudio=".$studio."'>".$v["studio"]."</a> (".$cnt.")<br>";
+	foreach ($result as $k => $v) {
 
-			if(count($alt_result) > 1) {
+		if ($v["studio"] != "") {
+			$cnt = $v["cnt"];
+
+			$query = 'select count(substudio) as cnt, substudio from ' . Db_TABLE_FILEDB . ' WHERE ' . $lib_where . ' studio like "' . $v['studio'] . '" GROUP by substudio ORDER BY `substudio` ASC';
+			$alt_result = $db->query($query);
+
+			$studio = str_replace(" ", "-", $v['studio']);
+			$studio = str_replace("/", "_", $studio);
+
+			echo "<li><a href='studio.php?studio=" . $studio . $lib_req . "'>" . $v["studio"] . "</a> (" . $cnt . ")<br>";
+
+			if (count($alt_result) > 1) {
 				echo "<ul>";
-			
-				foreach($alt_result as $k_a => $v_a )
-				{
-					if($v_a["studio_a"] != "" ){
+
+				foreach ($alt_result as $k_a => $v_a) {
+					if ($v_a["substudio"] != "") {
 						$cntv_a = $v_a["cnt"];
-						$studio_a = str_replace(" ","-",$v_a['studio_a']);
-						$studio_a = str_replace("/","_",$studio_a);
-						echo "<li><a href='studio.php?viewstudio=".$studio_a."'>".$v_a["studio_a"]."</a>(".$cntv_a.") <br>";
+						$substudio = str_replace(" ", "-", $v_a['substudio']);
+						$substudio = str_replace("/", "_", $substudio);
+						echo "<li><a href='studio.php?substudio=" . $substudio . $lib_req . "'>" . $v_a["substudio"] . "</a>(" . $cntv_a . ") <br>";
 					}
-					
 				}
 				echo "</ul>";
 			}
-			
 		}
 	}
-	echo "</ul><li><a href='studio.php?viewstudio=NULL'>Studio not Set</a><br>";
+	echo "</ul>";
 
- ?>
- </ul>
- </main>
- <?php include __LAYOUT_FOOTER__;  ?>
+	?>
+	</ul>
+</main>
+<?php include __LAYOUT_FOOTER__;  ?>
