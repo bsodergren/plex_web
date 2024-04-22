48717c93fa0202dc9d76817ec64bc385aeda9b7d
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-04-22 00:15:00 +0000

    dd

--- src/Config/urlpaths.php
+++ src/Config/urlpaths.php
@@ -2,4 +2,4 @@
 
 define('__URL_PATH__', APP_HOME);
 define('__URL_HOME__', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].__URL_PATH__);
-define('__LAYOUT_URL__', __URL_HOME__.'/assets/');
+define('__LAYOUT_URL__', __URL_HOME__.'/assets');
