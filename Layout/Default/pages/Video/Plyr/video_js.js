6820707d96d1de9de0a07fb4f6fc1898708678ed
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-06-08 18:05:02 -0500

    ch

--- Layout/Default/pages/Video/Plyr/video_js.js
+++ Layout/Default/pages/Video/Plyr/video_js.js
@@ -1,92 +1,63 @@
-// function rightClickSeekTooltip (event) {
-//     const skipTo = Math.round(
-//         // (event.offsetX / event.target.clientWidth) *
-//             parseInt(event.target.getAttribute('aria-valuenow'), 10)
-//     )
-
-//     addChapter(skipTo)
-//     const t = formatTime(skipTo)
-
-//     time = `${t.minutes}:${t.seconds}`
-// }
-
-
-
-// const player = new Plyr('video', {
-//     captions: { active: true },
-//     keyboard: { focused: true, global: true },
-//     hideControls: false,
-//     disableContextMenu: true,
-//     seekTime: 30,
-
-//     markers: { enabled: true, points: !!ChapterIndex!! }
-// })
-// window.player = player
-
-
-// function setPlayerTime(time){
-//     player.currentTime = time;
-// }
-
 function calculateAspectRatio(width, height) {
     function gcd(a, b) {
-        return (b == 0) ? a : gcd(b, a % b);
+        return b == 0 ? a : gcd(b, a % b);
     }
 
     var divisor = gcd(width, height);
 
-    return (width / divisor) + ":" + (height / divisor);
+    return width / divisor + ":" + height / divisor;
 }
 
-function calculateApectHeight(width, ratio)
-{
+function calculateApectHeight(width, ratio) {
     var t_arr = ratio.split(":");
     var div = width / t_arr[0];
     return div * t_arr[1];
 }
 
-function resizeWindow(large,width=1280,height=720)
-{
-    if(large == null)
-    {
-        large = false
+function resizeWindow(large, width = 1280, height = 720) {
+    if (large == null) {
+        large = false;
     }
 
-    if(typeof large == "object"){
-        large = true
+    if (typeof large == "object") {
+        large = true;
     }
 
-    videoWidth = width
-    videoHeight = height
+    videoWidth = width;
+    videoHeight = height;
 
-    text= "Origintal Video W:"+videoWidth + " H:" + videoHeight;
+    text = "Origintal Video W:" + videoWidth + " H:" + videoHeight;
     // console.log(text)
 
-    ratio = calculateAspectRatio(videoWidth, videoHeight)
+    ratio = calculateAspectRatio(videoWidth, videoHeight);
 
     if (videoWidth > 1921) {
-        videoWidth =  1280
+        videoWidth = 1280;
     }
 
+    windowWidth = videoWidth * 0.75;
+    videoHeight = calculateApectHeight(videoWidth, ratio);
 
-
-
-    windowWidth = videoWidth * 0.75
-  videoHeight = calculateApectHeight(videoWidth, ratio);
-
-    text= "Video to W:"+videoWidth + " H:" + videoHeight + " Ratio:" +  ratio ;
+    text = "Video to W:" + videoWidth + " H:" + videoHeight + " Ratio:" + ratio;
     // console.log(text)
-    heightMulti = .9
-     if(large == true) {
+    heightMulti = 0.9;
+    if (large == true) {
         // heightMulti = heightMulti + .13
-     }
-        windowHeight = videoHeight * heightMulti
+    }
+    windowHeight = videoHeight * heightMulti;
 
-    text= "Resising Window to W:"+windowWidth + " H:" + windowHeight + ", x"+heightMulti+" Playlist:"+large;
+    text =
+        "Resising Window to W:" +
+        windowWidth +
+        " H:" +
+        windowHeight +
+        ", x" +
+        heightMulti +
+        " Playlist:" +
+        large;
     // console.log(text)
 
-     window.resizeTo(windowWidth, windowHeight)
+    window.resizeTo(windowWidth, windowHeight);
 }
 
-resizeWindow(document.querySelector('.playlist-icon'),!!width!!,!!height!!)
-
+resizeWindow(document.querySelector(".playlist-icon"), { $width }, { $height });
